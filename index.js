84c95cd0af846b876b395990dd9d434d5c1434c4
Nguyen <danbaonguyen2001@gmail.com>
2022-06-29 10:07:07 +0700

    nguyen

--- index.js
+++ index.js
@@ -1,5 +1,35 @@
-import React from "react";
+//get modal  element
+//var modal = document.getElementById("modal");
+const clickLogin = document.querySelector(".js-btn-login");
+const clickRegister = document.querySelector(".js-btn-register");
+const clickCloseLogin = document.querySelector(".js-close-modal-login");
+const clickCloseRegister = document.querySelector(".js-close-modal-register");
 
-export default function index() {
-  return <div>index</div>;
+const modalLogin = document.querySelector(".modal__login");
+const modalRegister = document.querySelector(".modal__register");
+
+//get close modal element
+var closeModal = document.getElementById("close-modal");
+
+//Listen for clicks
+clickLogin.addEventListener("click", openLogin);
+function openLogin() {
+  modalLogin.classList.add("open");
+}
+
+clickRegister.addEventListener("click", openRegister);
+function openRegister() {
+  modalRegister.classList.add("open");
+}
+
+clickCloseLogin.addEventListener("click", closeModalLogin);
+function closeModalLogin() {
+  alert("Close Login");
+  modalLogin.classList.remove("open");
+}
+
+clickCloseRegister.addEventListener("click", closeModalRegister);
+function closeModalRegister() {
+  alert("Close Register");
+  modalRegister.classList.remove("open");
 }
