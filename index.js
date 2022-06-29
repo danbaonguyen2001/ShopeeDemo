3a31b5b2dec38f3b030c4a6465c8d5863c527900
Nguyen <danbaonguyen2001@gmail.com>
2022-06-29 11:11:08 +0700

    nguyen

--- index.js
+++ index.js
@@ -2,14 +2,21 @@
 //var modal = document.getElementById("modal");
 const clickLogin = document.querySelector(".js-btn-login");
 const clickRegister = document.querySelector(".js-btn-register");
+
 const clickCloseLogin = document.querySelector(".js-close-modal-login");
 const clickCloseRegister = document.querySelector(".js-close-modal-register");
 
+const clickSwitchToLogin = document.querySelector(".js-btn-switch-to-login");
+const clickSwitchToRegister = document.querySelector(
+  ".js-btn-switch-to-register"
+);
+
+const clickOutSideModal = document.querySelector(".modal");
+
 const modalLogin = document.querySelector(".modal__login");
 const modalRegister = document.querySelector(".modal__register");
-
-//get close modal element
-var closeModal = document.getElementById("close-modal");
+const modals = document.querySelectorAll(".modal");
+const modalBody = document.querySelector(".modal__body");
 
 //Listen for clicks
 clickLogin.addEventListener("click", openLogin);
@@ -24,12 +31,34 @@ function openRegister() {
 
 clickCloseLogin.addEventListener("click", closeModalLogin);
 function closeModalLogin() {
-  alert("Close Login");
   modalLogin.classList.remove("open");
 }
 
 clickCloseRegister.addEventListener("click", closeModalRegister);
 function closeModalRegister() {
-  alert("Close Register");
   modalRegister.classList.remove("open");
 }
+
+clickSwitchToLogin.addEventListener("click", switchToLogin);
+function switchToLogin() {
+  modalRegister.classList.remove("open");
+  modalLogin.classList.add("open");
+}
+
+clickSwitchToRegister.addEventListener("click", switchToRegister);
+function switchToRegister() {
+  modalLogin.classList.remove("open");
+  modalRegister.classList.add("open");
+}
+
+clickOutSideModal.addEventListener("click", outSideModal);
+function outSideModal() {
+  for (const modal of modals) {
+    if (modal.classList.contains("open")) modal.classList.remove("open");
+    else return;
+  }
+}
+
+modalBody.addEventListener("click", (event) => {
+  event.stopPropagation();
+});
